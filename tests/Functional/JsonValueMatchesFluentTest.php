a0ed77cb71e31f17f7bf50ced054ae1e21998474
Christoph Quadt <quadt@united-domains.de>
2020-03-02 11:58:32 +0100

    Fixed unit tests due to redeclared functions

--- tests/Functional/JsonValueMatchesFluentTest.php
+++ tests/Functional/JsonValueMatchesFluentTest.php
@@ -28,18 +28,18 @@ class JsonValueMatchesFluentTest extends TestCase
 
     public function testAssertThatJsonDocumentContainsJsonValue()
     {
-        assertThat(self::$exampleDocument, containsJsonValue('$.identifier', 1234));
+        $this->assertThat(self::$exampleDocument, containsJsonValue('$.identifier', 1234));
     }
 
     public function testAssertThatJsonDocumentMatchesJsonConstraints()
     {
-        assertThat(
+        $this->assertThat(
             self::$exampleDocument,
             matchesJsonConstraints(
                 [
-                    '$.owner.name' => equalTo('Max Mustermann'),
-                    '$.products[*].identifier' => greaterThanOrEqual(500),
-                    '$.products[*].name' => logicalNot(equalTo('Weißbrot'))
+                    '$.owner.name' => $this->equalTo('Max Mustermann'),
+                    '$.products[*].identifier' => $this->greaterThanOrEqual(500),
+                    '$.products[*].name' => $this->logicalNot($this->equalTo('Weißbrot'))
                 ]
             )
         );
